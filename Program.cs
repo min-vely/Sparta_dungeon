9b19cb789cd1344e124e0ccbaa505411385675d5
정민 <81990267+min-vely@users.noreply.github.com>
2023-11-10 16:24:03 +0900

    상태 보기에서 아이템 장착시 추가 능력치 표시되도록 수정

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ internal class Program
     // 아이템 장착 정보 유지용 리스트
     private static List<int> equippedItems = new List<int>();
 
-    Items[] items;
+    static Items[] items;
 
     static void Main(string[] args)
     {
@@ -28,7 +28,7 @@ internal class Program
         player = new Character("루루", "서포터", 1, 47, 26, 595, 1500);
 
         // 아이템 정보 세팅(배열 사용)
-        Items[] items = new Items[]
+        items = new Items[]
         {
         new Items("존야의 모래시계", "방어력", 45, "띵 - "),
         new Items("구인수의 격노검", "공격력", 30, "AD룰루 필수템"),
@@ -39,6 +39,7 @@ internal class Program
         };
 
         // switch문으로 배열을 변수에 할당
+        // 여긴 아이템 1 = 인덱스 0번!!!!!
         for (int i = 0; i < items.Length; i++)
         {
             switch (i)
@@ -92,37 +93,91 @@ internal class Program
 
     static void DisplayMyInfo()
     {
-        // 아이템 장착 여부에 따라 능력치 계산
-        int totalAtk = player.Atk;
-        int totalDef = player.Def;
-        int totalHp = player.Hp;
-
         Console.Clear();
-
-
-        
-        if (equippedItems.Contains(1))
-        {
-            totalDef += item1.AbilityValue;
-        }
-        if (equippedItems.Contains(2))
-        {
-            totalAtk += item2.AbilityValue;
-        }
 
         Console.WriteLine("상태보기");
         Console.WriteLine("캐릭터의 정보를 표시합니다.");
         Console.WriteLine();
         Console.WriteLine($"Lv.{player.Level}");
-        Console.WriteLine($"{player.Name}({player.Job})");
-        Console.WriteLine($"공격력: {totalAtk}");
-        Console.WriteLine($"방어력: {totalDef}");
-        Console.WriteLine($"체력: {totalHp}");
+        Console.WriteLine($"{player.Name} ( {player.Job} )");
+
+        // 장착한 아이템이 하나도 없다면
+        if (equippedItems.Count == 0)
+        {
+            // 기본 능력치 출력
+            Console.WriteLine($"공격력 : {player.Atk}");
+            Console.WriteLine($"방어력 : {player.Def}");
+            Console.WriteLine($"체력 : {player.Hp}");
+        }
+        // 장착한 아이템이 하나 이상 존재한다면
+        else
+        {
+            int bonusAtk = 0;
+            int bonusDef = 0;
+            int bonusHp = 0;
+
+            // 장착한 아이템 리스트에 1~6번이 들어가 있다면
+            foreach (int itemIndex in equippedItems)
+            {
+                // 아이템 인덱스로 실제 아이템을 가져옴
+                Items equippedItem = items[itemIndex];
+
+                // 아이템으로 얻은 능력치를 계산
+                if (equippedItem.AbilityName == "공격력")
+                {
+                    bonusAtk += equippedItem.AbilityValue;
+                }
+                else if (equippedItem.AbilityName == "방어력")
+                {
+                    bonusDef += equippedItem.AbilityValue;
+                }
+                else if (equippedItem.AbilityName == "체력")
+                {
+                    bonusHp += equippedItem.AbilityValue;
+                }
+            }
+
+            // 아이템으로 얻은 능력치가 0보다 큰 경우에만 표시
+            if (bonusAtk > 0)
+            {
+                Console.WriteLine($"공격력 : {player.Atk + bonusAtk} (+{bonusAtk})");
+            }
+            // 공격력 아이템을 장착하지 않았다면 기본 능력치 표시
+            else
+            {
+                Console.WriteLine($"공격력 : {player.Atk}");
+            }
+            if (bonusDef > 0)
+            {
+                Console.WriteLine($"방어력 : {player.Def + bonusDef} (+{bonusDef})");
+            }
+            else
+            {
+                Console.WriteLine($"방어력 : {player.Def}");
+            }
+            if (bonusHp > 0)
+            {
+                Console.WriteLine($"체력 : {player.Hp + bonusHp} (+{bonusHp})");
+            }
+            else
+            {
+                Console.WriteLine($"체력 : {player.Hp}");
+            }
+        }
+
+
+
+
+        //// 총 능력치 출력
+        //int totalAtk = player.Atk + bonusAtk;
+        //int totalDef = player.Def + bonusDef;
+        //int totalHp = player.Hp + bonusHp;
+
         Console.WriteLine($"Gold : {player.Gold} G");
         Console.WriteLine();
         Console.WriteLine("0. 나가기");
         Console.WriteLine();
-        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        Console.WriteLine("원하시는 행동을 입력해주세요");
 
         int input = CheckValidInput(0, 0);
         switch (input)
@@ -131,9 +186,8 @@ internal class Program
                 DisplayGameIntro();
                 break;
         }
-
-
     }
+
 
     static void DisplayInventory()
     {
@@ -145,10 +199,13 @@ internal class Program
         Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
         Console.WriteLine();
         Console.WriteLine("[아이템 목록]");
-        Console.WriteLine($"- {(equippedItems.Contains(1) ? "[E]" : "")}{item1.ItemName}      | {item1.AbilityName} +{item1.AbilityValue} | {item1.ItemInfo}");
-        Console.WriteLine($"- {(equippedItems.Contains(2) ? "[E]" : "")}{item2.ItemName}      | {item2.AbilityName} +{item2.AbilityValue} | {item2.ItemInfo}");
-        Console.WriteLine();
 
+        for (int i = 0; i < items.Length; i++)
+        {
+            Console.WriteLine($"- {(equippedItems.Contains(i) ? "[E]" : "")}{items[i].ItemName}      | {items[i].AbilityName} +{items[i].AbilityValue} | {items[i].ItemInfo}");
+        }
+
+        Console.WriteLine();
         Console.WriteLine("1. 장착 관리");
         Console.WriteLine("0. 나가기");
         Console.WriteLine();
@@ -175,25 +232,49 @@ internal class Program
         Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
         Console.WriteLine();
         Console.WriteLine("[아이템 목록]");
-        Console.WriteLine($"- 1 {(equippedItems.Contains(1) ? "[E]" : "")}{item1.ItemName}      | {item1.AbilityName} +{item1.AbilityValue} | {item1.ItemInfo}");
-        Console.WriteLine($"- 2 {(equippedItems.Contains(2) ? "[E]" : "")}{item2.ItemName}      | {item2.AbilityName} +{item2.AbilityValue} | {item2.ItemInfo}");
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            Console.WriteLine($"- {i + 1} {(equippedItems.Contains(i) ? "[E]" : "")}{items[i].ItemName}      | {items[i].AbilityName} +{items[i].AbilityValue} | {items[i].ItemInfo}");
+        }
+
         Console.WriteLine();
         Console.WriteLine("0. 나가기");
         Console.WriteLine();
         Console.WriteLine("원하시는 행동을 입력해주세요.");
 
-        int input = CheckValidInput(0, 2);
+        int input = CheckValidInput(0, 6);
+        // 사용자 입력값에서 1을 빼서 아이템의 인덱스로 변환
+        int itemIndex = input - 1;
+
+        // 여긴 아이템 1 = 인덱스 1번!!!!!
         switch (input)
         {
             case 0:
                 DisplayGameIntro();
                 break;
             case 1:
-                ItemEquipped(equippedItems, 1);
+                ItemEquipped(equippedItems, itemIndex);
                 EquipItems(equippedItems);
                 break;
             case 2:
-                ItemEquipped(equippedItems, 2);
+                ItemEquipped(equippedItems, itemIndex);
+                EquipItems(equippedItems);
+                break;
+            case 3:
+                ItemEquipped(equippedItems, itemIndex);
+                EquipItems(equippedItems);
+                break;
+            case 4:
+                ItemEquipped(equippedItems, itemIndex);
+                EquipItems(equippedItems);
+                break;
+            case 5:
+                ItemEquipped(equippedItems, itemIndex);
+                EquipItems(equippedItems);
+                break;
+            case 6:
+                ItemEquipped(equippedItems, itemIndex);
                 EquipItems(equippedItems);
                 break;
         }
